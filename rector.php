41d844919a62c01c504079dc5fd477568eb9e60c
Niels Drost <nielsdrost7@gmail.com>
2025-02-09 06:09:58 +0100

    rector config dead code

--- rector.php
+++ rector.php
@@ -1,27 +1,20 @@
 <?php
 
-use Rector\Arguments\Rector\FuncCall\FunctionArgumentDefaultValueReplacerRector;
 use Rector\Config\RectorConfig;
-use Rector\Php73\Rector\BooleanOr\IsCountableRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Php80\Rector\Class_\StringableForToStringRector;
-use Rector\Php80\Rector\FuncCall\ClassOnObjectRector;
-use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
-use Rector\Php81\Rector\Class_\SpatieEnumClassToEnumRector;
-use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
 
 return RectorConfig::configure()
     ->withSkip([
         'app/Providers/AppServiceProvider.php',
-        ClassOnObjectRector::class,
-        ClassPropertyAssignToConstructorPromotionRector::class,
-        FunctionArgumentDefaultValueReplacerRector::class,
-        IsCountableRector::class,
-        MixedTypeRector::class,
-        StringableForToStringRector::class,
-        NullToStrictStringFuncCallArgRector::class,
-        SpatieEnumClassToEnumRector::class,
+        Rector\Arguments\Rector\FuncCall\FunctionArgumentDefaultValueReplacerRector::class,
+        Rector\Php73\Rector\BooleanOr\IsCountableRector::class,
+        Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class,
+        Rector\Php80\Rector\Class_\StringableForToStringRector::class,
+        Rector\Php80\Rector\FunctionLike\MixedTypeRector::class,
+        Rector\Php80\Rector\FuncCall\ClassOnObjectRector::class,
+        Rector\Php81\Rector\Class_\SpatieEnumClassToEnumRector::class,
+        Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
     ])
     ->withPaths([
         __DIR__ . '/app',
@@ -32,7 +25,7 @@ return RectorConfig::configure()
         ReturnTypeFromStrictTypedPropertyRector::class,
     ])
     ->withSets([
-        //SetList::DEAD_CODE,
+        SetList::DEAD_CODE,
         //SetList::CODE_QUALITY,
         //SetList::CODING_STYLE,
         //SetList::EARLY_RETURN,
