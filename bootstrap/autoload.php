b2ca752c31e643f2ca66a1fc4e638b69b4e5b4ae
Niels Drost <nielsdrost7@gmail.com>
2025-02-16 11:13:44 +0100

    cleanup

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,7 +3,7 @@
 // https://laracasts.com/discuss/channels/general-discussion/l5-maximum-function-nesting-level-of-100-reached-aborting/
 ini_set('xdebug.max_nesting_level', 256);
 
-define('LARAVEL_START', microtime(true));
+//define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
